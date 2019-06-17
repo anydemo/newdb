d140e18432771c7b409fdde501ac8bcbaad363c9
exfly <exflyg@gmail.com>
2019-06-17 12:39:45 +0800

    fixed NewTxID

--- tx.go
+++ tx.go
@@ -14,16 +14,8 @@ type TxID struct {
 
 // NewTxID new one *TxID
 func NewTxID() *TxID {
-	var id uint64
-	var retry = true
-	for retry {
-		var oID = txIDPool
-		id = oID + 1
-		retry = !atomic.CompareAndSwapUint64(&txIDPool, oID, id)
-		txL.WithField("old_id", oID).WithField("new_id", id).Debugf("retry: %v", retry)
-	}
 	return &TxID{
-		ID: id,
+		ID: atomic.AddUint64(&txIDPool, 1),
 	}
 }
 
